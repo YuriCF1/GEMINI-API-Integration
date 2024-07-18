1cbb6cfd3a9c8c29c87c068ebaaf9165d13caa92
Yuri Cruz <yuricruzf@gmail.com>
2024-07-18 17:34:41 -0300

    Improving prompt and changing model

--- index.js
+++ index.js
@@ -25,9 +25,9 @@ const apiKey = process.env.GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-pro",
-  systemInstruction:
-    "Você é o chatbot de um site que vende pacotes de viagem. Ao ser perguntado sobre algum destino, como bairro, cidade, estado, país, continente e pontos turísticos diversos, você poderá fornecer informações. Caso seja perguntado sobre algo que não ter relação com viagem e turismo, informe que não poder responder a essa dúvida.\n\nPara formular a resposta, quero que os tópicos apareçam como lista com marcadores e sempre deve conter as categorias: características, localização, cultura, pontos turísticos e culinária.",
+  model: "gemini-1.5-flash",
+  system_instruction:
+    "Você é o chatbot de um site que vende pacotes de viagem. Ao ser perguntado sobre algum destino, como bairro, cidade, estado, país, continente e pontos turísticos diversos, você poderá fornecer informações. Caso seja perguntado sobre algo que não ter relação com viagem e turismo, informe que não poder responder a essa dúvida. Para formular a resposta, quero que os tópicos apareçam como lista com marcadores e sempre deve conter as categorias que forem solicitadas no momento da pergunta. Alguns exemplos de categoria: características, localização, cultura, pontos turísticos, clima, dicas, como chegar, curiosidades e culinária. Caso o peçam para você falar tudo sobre o local, fale algo a repeito de todas essas categorias que usei para exemplificar.",
 });
 
 const generationConfig = {
@@ -60,11 +60,18 @@ async function run() {
       },
     ],
   });
+
   let prompt = await fazerPergunta(
     "Me fale sobre o destino que deseja conhecer: "
   );
 
-  const result = await chatSession.sendMessage(prompt);
+  const categorias = await fazerPergunta(
+    "Me fale as categorias que tu desejas visualizar sobre um determinado destino: "
+  );
+
+  const result = await chatSession.sendMessage(
+    `Me fale sobre as categeorias ${categorias} sobre o destino ${prompt}`
+  );
   console.log(result.response.text());
 }
 
