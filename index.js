c38d5b957233cc0b93b9ae1469e21ca559a6d5e6
Yuri Cruz <yuricruzf@gmail.com>
2024-07-22 10:37:01 -0300

    Adding evaluate function to options

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import { fazerPergunta } from "./pergunta.js";
 import { perguntar } from "./perguntaLivre.js";
 import { consultar } from "./consultaDestino.js";
 import { analisaImagem } from "./geraTextoPorImagem.js";
+import { processaArquivoTexto } from "./categorizador.js";
 
 async function principal() {
   const escolha = await fazerPergunta(`Escolha uma das opções abaixo: \n
   1. Fazer uma pergunta livre sobre um destino;
   2. Comparação de destinos por categorias;
   3. Ver informações com base em uma imagem;
+  4. Fazer a análise de sentimentos baseado em arquivo texto
   \nOpção desejada: `);
 
   if (escolha === "1") {
@@ -19,6 +21,8 @@ async function principal() {
       "\nMe informe o caminho completo e nome da imagem: "
     );
     await analisaImagem(imagem);
+  } else if (escolha === "4") {
+    await processaArquivoTexto();
   } else {
     console.log("Escolha inválida.");
   }
